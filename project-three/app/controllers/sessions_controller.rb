8ce8ffc9ab25e3fdc1d91f305755e7e48e4296ab
brookekfox <brookekfox@gmail.com>
2015-03-10 16:50:23 -0700

    show/hide links on public page depending on who is logged in

---
+++ project-three/app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+	def new
+		@user = User.new
+	end
+
+	def create
+		user = User.where(email: params[:user][:email]).first
+		if user != nil && user.authenticate(params[:user][:password])
+			session['user_id']   = user.id.to_s
+			user.is_current_user = true
+			redirect_to recommendations_path
+		else
+			redirect_to new_sessions_path
+		end
+	end
+
+	def destroy
+		session.destroy
+		@user.is_current_user = false
+		redirect_to root_path
+	end
+
+end
